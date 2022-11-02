5c654948aa92f882f4aa86d768b7cc20d895d782
Dmytro Hushchyk <107995555+DHushchyk@users.noreply.github.com>
2022-11-02 17:41:15 +0200

    Prepare progect to deploying

--- url_shortening_service/settings.py
+++ url_shortening_service/settings.py
@@ -30,7 +30,7 @@ SECRET_KEY = os.environ.get(
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.environ.get("DJANGO_DEBUG", "") != "False"
 
-ALLOWED_HOSTS = ["127.0.0.1", "url-shortener-dh.herokuapp.com"]
+ALLOWED_HOSTS = ["127.0.0.1", "'short-a.herokuapp.com"]
 
 
 # Application definition
